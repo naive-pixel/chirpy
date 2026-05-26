1ad0b7d5a9bc5f1c7da0fd0162bd25975f8866cf
naive-pixel <erlentoi@outlook.com>
2026-05-26 15:16:59 +0200

    add metrics, admin page and http test files

--- main.go
+++ main.go
@@ -3,18 +3,43 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
-	srv.ListenAndServe()
+	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+
 }
---
+++ metrics.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (acfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		acfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+/*
+func (acfg *apiConfig) getNumberOfRequestsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", acfg.fileserverHits.Load())
+
+}*/
+
+func (acfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	acfg.fileserverHits.Store(0)
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Reset counter"))
+}
+
+func (acfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	welcomePage := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>",
+		acfg.fileserverHits.Load())
+	fmt.Fprintf(w, welcomePage)
+}
